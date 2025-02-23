34eafa559b6aafb5927356e9b3ff2912c43a861b
vini-barbo <vinicius2508@hotmail.com>
2025-02-23 17:40:27 -0300

    feature: sidebar done

--- src/app/layout/saas/sidebar/sidebar.component.ts
+++ src/app/layout/saas/sidebar/sidebar.component.ts
@@ -1,10 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TuiIcon, TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [TuiIcon, CommonModule, TuiButton],
   template: `
     <div
+      class="fixed flex h-full  flex-col  bg-[#188083] justify-between px-4 text-white shadow-xl shadow-blue-gray-900/5">
+      <div class="flex flex-col gap-5 pt-3">
+        <a
+          ariaLabel
+          [type]="'button'"
+          tuiButton
+          *ngFor="let item of topIcons"
+          [iconStart]="item.icon"
+          class="!bg-transparent hover:!bg-white hover:!text-[#188083]  active:!bg-white active:!text-[#188083]">
+        </a>
+      </div>
+      <div>
+        <hr class="my-2 border-blue-gray-50" />
+        <div class="flex flex-col gap-5">
+          <a
+            ariaLabel
+            [type]="'button'"
+            tuiButton
+            *ngFor="let item of bottonIcons"
+            [iconStart]="item.icon"
+            class="!bg-transparent hover:!bg-white hover:!text-[#188083]  active:!bg-white active:!text-[#188083]">
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div
       class="fixed flex h-full  flex-col  bg-[#188083] bg-clip-border  text-white shadow-xl shadow-blue-gray-900/5">
       <nav
         class="flex h-full  min-w-[240px] flex-col justify-between gap-1 p-2  py-5 font-sans text-base font-normal text-white ">
@@ -123,8 +152,19 @@ import { Component } from '@angular/core';
           </div>
         </div>
       </nav>
-    </div>
+    </div> -->
   `,
   styles: ``,
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  topIcons = [
+    { path: '/reviews', icon: '@tui.archive', title: 'Reviews' },
+    { path: '/teams', icon: '@tui.users', title: 'Teams' },
+  ];
+  bottonIcons = [
+    { path: '/profile', icon: '@tui.user', title: 'Profile' },
+    { path: '/settings', icon: '@tui.settings', title: 'Settings' },
+    { path: '/logout', icon: '@tui.log-out', title: 'Log Out' },
+    { path: '/logout', icon: '@tui.log-out', title: 'Log Out' },
+  ];
+}
