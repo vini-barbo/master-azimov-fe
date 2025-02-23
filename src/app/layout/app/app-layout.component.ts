12fdb8c84b540f3ae33529993f9a47ebd3fd7e12
vini-barbo <vinicius2508@hotmail.com>
2025-02-23 10:50:36 -0300

    feature: create router

---
+++ src/app/layout/app/app-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [],
+  template: ` <p>main-layout works!</p> `,
+  styles: ``,
+})
+export class MainLayoutComponent {}
