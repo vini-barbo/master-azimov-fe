5bfbabe64a7d4134ec31b40278c7386046c56a42
vini-barbo <vinicius2508@hotmail.com>
2025-02-24 16:43:54 -0300

    feat: add forms

--- src/app/page/review/identification/identification.component.ts
+++ src/app/page/review/identification/identification.component.ts
@@ -1,9 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiAppearance, TuiTextfield } from '@taiga-ui/core';
+import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-identification',
-  imports: [],
-  template: ` <p>identification works!</p> `,
+  imports: [TuiCardLarge, TuiAppearance, TuiForm, TuiTextfield, CommonModule],
+  template: ` <form class="w-full h-full !grid !grid-cols-2" tuiForm="m">
+    <aside class="flex flex-col gap-2">
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+    </aside>
+    <aside class="flex flex-col gap-2">
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+      <tui-textfield>
+        <label tuiLabel>Name</label>
+        <input formControlName="name" placeholder="John Wick" tuiTextfield />
+      </tui-textfield>
+    </aside>
+  </form>`,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
