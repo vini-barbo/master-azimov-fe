0352b561d2ae8d005e49bb805d36b71daf65ba88
vini-barbo <vinicius2508@hotmail.com>
2025-03-11 09:58:04 -0300

    feature: add new table

--- src/app/page/review/result/result.component.ts
+++ src/app/page/review/result/result.component.ts
@@ -1,22 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ResultTableComponent } from './result-table/result-table.component';
-import { ResultPaginationComponent } from './result-pagination/result-pagination.component';
-import { ResultControlsComponent } from './result-controls/result-controls.component';
+// import { ResultPaginationComponent } from './result-pagination/result-pagination.component';
+// import { ResultControlsComponent } from './result-controls/result-controls.component';
+
+// ResultPaginationComponent,
+// ResultControlsComponent,
 
 @Component({
   selector: 'app-result',
-  imports: [
-    ResultTableComponent,
-    ResultPaginationComponent,
-    ResultControlsComponent,
-  ],
+  imports: [ResultTableComponent],
   template: `
     <main class="flex flex-col gap-4">
-      <section><app-result-controls></app-result-controls></section>
+      <!-- <section><app-result-controls></app-result-controls></section> -->
       <section>
         <app-result-table class="w-full"></app-result-table>
       </section>
-      <section><app-result-pagination></app-result-pagination></section>
+      <!-- <section><app-result-pagination></app-result-pagination></section> -->
     </main>
   `,
   styles: ``,
